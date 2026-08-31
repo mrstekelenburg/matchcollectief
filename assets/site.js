1a9f85d2d2cfc14ee235a86fb453f93ae0d9ae32
Remorec <info@remorec.com>
2026-08-31 14:31:46 +0200

    Delete assets directory

--- assets/site.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function(){
-  const toggle = document.querySelector('.menu-toggle');
-  const links = document.querySelector('.nav-links');
-  if(toggle && links){
-    toggle.addEventListener('click', () => links.classList.toggle('open'));
-  }
-});
