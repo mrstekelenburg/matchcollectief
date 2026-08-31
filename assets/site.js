31ac00e5c8fdfd5d64714aedff0597d278f5cb24
Remorec <info@remorec.com>
2026-08-31 14:32:27 +0200

    Add files via upload

---
+++ assets/site.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+  const toggle = document.querySelector('.menu-toggle');
+  const links = document.querySelector('.nav-links');
+  if(toggle && links){
+    toggle.addEventListener('click', () => links.classList.toggle('open'));
+  }
+});
